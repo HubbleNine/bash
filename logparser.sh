6c2acf9a223c1d57504e2e0bf40be700dc68463d
HubbleNine <42842904+HubbleNine@users.noreply.github.com>
2018-12-11 11:06:47 -0600

    Update logparser.sh

--- logparser.sh
+++ logparser.sh
@@ -3,15 +3,18 @@
 #Author: Me | Senior Network Engineer
 #Date: 12/11/2018
 #This script is designed to be run from crontab at some frequency and look for a particular string
-#in whatever log you need
+#in whatever log you need, then send that log line via email to someone
 
+#replace for whichever string you want to parse the logs for
 string=foobar
 
+#replace 'somelog' with the log name you want to parse
 tail -n 50 /var/log/somelog | \
 while read LINE
 do
   if echo "$LINE" | grep "$string" 1>/dev/null 2>&1
   then
-    printf "Bounce found on $HOSTNAME:\n\n$LINE" | mailx -s "Bounced Email on SMTPRelay" -r "SenderName<sendername@domain.com>" recipient@domain.com
+	#replace Subject Line, SenderName, sendername@domain, and recipient@domain with your entries
+    printf "Bounce found on $HOSTNAME:\n\n$LINE" | mailx -s "Subject Line" -r "SenderName<sendername@domain.com>" recipient@domain.com
   fi
 done
